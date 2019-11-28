cb5f159361f4f82f1e3a2579649ef8d0612129f8
haranrk <haranrajkumar97@gmail.com>
2019-11-28 15:23:14 +0530

    Fix spelling error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='netacess',  
+     name='netaccess',  
      version='0.0.1',
      author="Haran Rajkumar",
      author_email="haranrajkumar97@gmail.com",
