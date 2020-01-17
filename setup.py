2612ae8a203b43ac545cc439e56e5bbd4ae33c0c
haranrk <haranrajkumar97@gmail.com>
2020-01-17 14:50:36 +0530

    Add required __init__ file
    Version bump to 1.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import os
 import setuptools
+import netaccess
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 setuptools.setup(
      name='netaccess',  
-     version='1.0.1',
+     version=netaccess.__version__,
      author="Haran Rajkumar",
      author_email="haranrajkumar97@gmail.com",
      description="CLI for IIT Madras netaccess",
@@ -16,14 +15,13 @@ setuptools.setup(
      install_requires = [
          'mechanize',
          ],
-     entry_points={
-         'console_scripts':['netaccess=netaccess.netaccess:main']
-     },
+     entry_points='''
+         [console_scripts]
+         netaccess=netaccess.netaccess:main
+     ''',
      classifiers=[
          "Programming Language :: Python :: 3.5",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
-     # include_package_data=True,
-     # zip_safe=False,
  )
